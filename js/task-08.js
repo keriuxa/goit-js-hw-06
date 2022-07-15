c9888d4991db366ab5eb0555b19402a432ce5cb4
keriuxa <67189463+keriuxa@users.noreply.github.com>
2022-07-15 19:14:21 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
 
+const loginForm = document.querySelector(".login-form")
+
+function userInfo(event) {
+    event.preventDefault();
+    const  inputs = { 
+        email: `${event.currentTarget.email.value}`,
+        password: `${event.currentTarget.password.value}` 
+    }
+
+    if (inputs.email ==="" || inputs.password ===""){
+        return alert("Please fill the form!")
+    }
+    return console.log(inputs)
+
+}
+ 
+loginForm.addEventListener('submit', userInfo)
+
