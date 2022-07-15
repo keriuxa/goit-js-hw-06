e5fc07813fbec46ad101517efad52fc40e63e26f
keriuxa <67189463+keriuxa@users.noreply.github.com>
2022-07-15 23:27:23 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,16 +7,11 @@ const ingredients = [
   'Condiments',
 ];
 
-const accItems = [];
+const itemList = ingredients.map(ingredient => {
+  const newItem = document.createElement("li");
+  newItem.textContent = ingredient;
+  newItem.classList.add(`item`);
+  return newItem
+});
 
-ingredients.forEach(function(ingredient)
-{
-
-const newItem = document.createElement("li");
-newItem.textContent = ingredient;
-newItem.classList.add(`item`);
-accItems.push(newItem);
-})
-
-
-document.querySelector('#ingredients').append(...accItems);
+document.querySelector('#ingredients').append(...itemList);
