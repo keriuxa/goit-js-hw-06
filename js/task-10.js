7b5f763a894a4dcbf3ee74001ad3a2eae26853c1
keriuxa <67189463+keriuxa@users.noreply.github.com>
2022-07-16 01:16:03 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const control = document.querySelector('#controls')
+const boxes = document.querySelector('#boxes')
+const input = document.querySelector('input')
+const createBtn = document.querySelector('button[data-create]')
+const destroyBtn = document.querySelector('button[data-destroy]')
+
+const createBoxes = (event)=> {
+  const curentCount = input.value
+  let boxSizes = 20
+  for (let i=0; i<curentCount; i+=1) {
+   
+    boxSizes = boxSizes +10;   
+    const boxCreate = document.createElement('div')
+          boxCreate.style.height = `${boxSizes}px`
+          boxCreate.style.width = `${boxSizes}px`
+          boxCreate.style.backgroundColor = getRandomHexColor()
+          boxes.append(boxCreate)
+  }
+  }
+
+const destroyBoxes = (event)=> {
+  boxes.innerHTML = '' 
+  input.value = ''
+}
+
+createBtn.addEventListener('click', createBoxes)
+destroyBtn.addEventListener('click', destroyBoxes)
