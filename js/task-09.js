15dab423b08d8188f3acb9e044dccd1d278564fe
keriuxa <67189463+keriuxa@users.noreply.github.com>
2022-07-15 19:14:24 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body")
+const button = document.querySelector('.change-color')
+const color = document.querySelector('.color')
+
+function changeBackgroundColor (event) {
+  body.style.backgroundColor = getRandomHexColor ();
+  color.textContent = body.style.backgroundColor;
+}
+
+button.addEventListener('click', changeBackgroundColor)
