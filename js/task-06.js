017613213d7fce7fdac5bfb6f3e3ab38f579141a
keriuxa <67189463+keriuxa@users.noreply.github.com>
2022-07-16 00:06:08 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,9 @@ const input = document.querySelector("#validation-input")
 function checkImput (event) {
   
     if (event.currentTarget.value.length !== Number(input.dataset.length)) {
-      return  event.currentTarget.classList.add('invalid')
+      return  event.currentTarget.classList.contains('valid') 
+      ? event.currentTarget.classList.replace('valid', 'invalid')
+      :event.currentTarget.classList.add('invalid')
     }
     return  event.currentTarget.classList.replace('invalid','valid')
 }
