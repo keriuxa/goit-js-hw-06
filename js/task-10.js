3d81c0f73b7cce8e7dfd73e96f669a20b5af78c9
keriuxa <67189463+keriuxa@users.noreply.github.com>
2022-07-19 13:43:40 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -8,24 +8,28 @@ const input = document.querySelector('input')
 const createBtn = document.querySelector('button[data-create]')
 const destroyBtn = document.querySelector('button[data-destroy]')
 
-const createBoxes = (event)=> {
-  const curentCount = input.value
-  let boxSizes = 20
-  for (let i=0; i<curentCount; i+=1) {
-   
-    boxSizes = boxSizes +10;   
-    const boxCreate = document.createElement('div')
-          boxCreate.style.height = `${boxSizes}px`
-          boxCreate.style.width = `${boxSizes}px`
-          boxCreate.style.backgroundColor = getRandomHexColor()
-          boxes.append(boxCreate)
-  }
-  }
+const createBoxes = (amount) =>{
+    const newBoxes = [];
+    let boxSizes = 20
+    for (let i=0; i<amount; i+=1) {
+      boxSizes = boxSizes +10;   
+      const boxCreate = document.createElement('div')
+            boxCreate.style.height = `${boxSizes}px`
+            boxCreate.style.width = `${boxSizes}px`
+            boxCreate.style.backgroundColor = getRandomHexColor()
+            newBoxes.push(boxCreate)
+}
+boxes.append(...newBoxes)
+}
 
 const destroyBoxes = (event)=> {
   boxes.innerHTML = '' 
   input.value = ''
 }
 
-createBtn.addEventListener('click', createBoxes)
+const addBoxes = (event) => {
+ createBoxes(input.value)
+}
+
+createBtn.addEventListener('click', addBoxes)
 destroyBtn.addEventListener('click', destroyBoxes)
